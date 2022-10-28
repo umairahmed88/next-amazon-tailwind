277e980b19b7a223a33f036b9480307b54abc584
umairahmed88 <umairahmed_88@ymail.com>
2022-10-29 01:33:45 +0500

    added login API

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,14 @@
 import React, { useContext, useEffect, useState } from "react";
 import Head from "next/head";
 import Link from "next/link";
+import { ToastContainer } from "react-toastify";
 import { Store } from "../utils/Store";
+import { useSession } from "next-auth/react";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Layout({ title, children }) {
+	const { status, data: session } = useSession();
+
 	const { state, dispatch } = useContext(Store);
 	const { cart } = state;
 
@@ -20,6 +25,7 @@ export default function Layout({ title, children }) {
 				<meta name='description' content='Ecommerce Website' />
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
+			<ToastContainer position='bottom-center' limit={1} />
 			<div className='flex min-h-screen flex-col justify-between'>
 				<header>
 					<nav className='flex h-12 justify-between shadow-md px-4 items-center'>
@@ -37,9 +43,15 @@ export default function Layout({ title, children }) {
 									)}
 								</a>
 							</Link>
-							<Link href='/login'>
-								<a className='p-2'>Login</a>
-							</Link>
+							{status === "loading" ? (
+								"loading"
+							) : session?.user ? (
+								session.user.name
+							) : (
+								<Link href='/login'>
+									<a className='p-2'>Login</a>
+								</Link>
+							)}
 						</div>
 					</nav>
 				</header>
