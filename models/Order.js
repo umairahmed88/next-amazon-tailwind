48bdb9e7001021ccf46e91ef3a4c378121a35282
umairahmed88 <umairahmed_88@ymail.com>
2022-10-31 14:27:56 +0500

    integrated Paypal feature to the app

--- models/Order.js
+++ models/Order.js
@@ -19,6 +19,7 @@ const orderSchema = new mongoose.Schema(
 			country: { type: String, required: true },
 		},
 		paymentMethod: { type: String, required: true },
+		paymentResult: { id: String, status: String, email_address: String },
 		itemsPrice: { type: String, required: true },
 		shippingPrice: { type: String, required: true },
 		taxPrice: { type: String, required: true },
